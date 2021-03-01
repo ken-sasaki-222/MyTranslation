7694ba8b476c08c4978582eaad17688ec9885401
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-02 00:03:55 +0900

    update create-Translation
    UIパーツのインスタンスを作成し、Storyboardと紐付け

---
+++ MyTranslation/Controller/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MyTranslation
+//
+//  Created by 佐々木　謙 on 2021/03/01.
+//
+
+import UIKit
+
+// テキスト入力による翻訳をおこなうクラス
+class HomeViewController: UIViewController {
+    
+    // MARK: プロパティ
+    // 翻訳前のテキストビュー
+    @IBOutlet weak var beforTextView: UITextView!
+    
+    // 翻訳後のテキストビュー
+    @IBOutlet weak var afterTextView: UITextView!
+    
+    // 翻訳を開始するボタン
+    @IBOutlet weak var startTranslationButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    // MARK: 翻訳開始ボタン
+    // 翻訳開始をタップすると呼ばれる
+    @IBAction func tapStartTranslationButton(_ sender: Any) {
+        print("翻訳開始")
+    }
+    
+}
+
