d6f4cf66d8949e572d4d42bf1b3b05cef75b1f31
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-02 14:33:12 +0900

    update create-Translation
    カウントは多く扱うので集約

---
+++ MyTranslation/Model/IntegerGroup/Count.swift
@@ -0,0 +1,14 @@
+//
+//  Count.swift
+//  MyTranslation
+//
+//  Created by 佐々木　謙 on 2021/03/02.
+//
+
+import Foundation
+
+// JSON解析などで扱うカウントの整数を集約したクラス
+class Count {
+    
+    static var zero = 0
+}
