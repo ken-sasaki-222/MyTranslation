bbf0b6783080964811bbfb1ece99073c20c6337e
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-04 13:19:27 +0900

    update create-RecordVC
    カスタムセルを用いてテキストの量で可変するセルを作成。Viewの階層を編集

---
+++ MyTranslation/View/RecordCellGroup/RecordCell.swift
@@ -0,0 +1,30 @@
+//
+//  RecordCell.swift
+//  MyTranslation
+//
+//  Created by 佐々木　謙 on 2021/03/04.
+//
+
+import UIKit
+
+// RecordCellを扱うクラス
+class RecordCell: UITableViewCell {
+   
+    // 履歴を表示するラベル
+    @IBOutlet weak var recordLabel: UILabel!
+    
+    // セル本体
+    @IBOutlet weak var recordCell: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
