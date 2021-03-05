7dc9ecf0d6b18cd78637e801c0d149ebf7be350e
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-05 17:14:35 +0900

    update create-MenuVC
    メニューページを扱うクラスを作成し、Storyboardと紐付け。

---
+++ MyTranslation/Controller/MenuGroup/MenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuViewController.swift
+//  MyTranslation
+//
+//  Created by 佐々木　謙 on 2021/03/05.
+//
+
+import UIKit
+
+// メニューページを扱うクラス
+class MenuViewController: UIViewController {
+
+    // MARK: - プロパティ
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
