402940d114e917e146fe3a3424fcd1e91ea04478
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-06 15:27:44 +0900

    update create-MenuVC
    管理する整数を追加

--- MyTranslation/Model/IntegerGroup/Count.swift
+++ MyTranslation/Model/IntegerGroup/Count.swift
@@ -10,7 +10,8 @@ import Foundation
 // JSON解析などで扱うカウントの整数を集約したクラス
 class Count {
     
-    static var zero = 0
-    static var one  = 1
-    static var two  = 2
+    static var zero  = 0
+    static var one   = 1
+    static var two   = 2
+    static var three = 3
 }
